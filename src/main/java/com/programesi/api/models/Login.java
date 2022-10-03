86b50aa4b92c45925f2646e470ceee2a1e2b7cb6
Lucas Oliveira <lucas.sales@justa.com.vc>
2022-10-03 18:39:26 -0300

    initial: :tada: initial commit

---
+++ src/main/java/com/programesi/api/models/Login.java
@@ -0,0 +1,47 @@
+package com.programesi.api.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+
+@Entity
+@Table(name = "pgs_log_login")
+public class Login {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "log_login")
+    private String login;
+
+    @Column(name = "log_password")
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/test/java/com/programesi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programesi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
